4e671f83e31a78777484da910dec06da402529d9
loopzser <124665029+loopzser@users.noreply.github.com>
2025-09-03 19:16:38 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,11 +5,11 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "2UpnxD5L#rXbmOhdsAgLXV8qjerEoa7CRDyj4HTEhv1fwcmJ7els", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "OFp02aLZ#51z2ksffU4P_apaSSpkq-yp09rxpUPUUIzr-niY0J7k", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 MODE: process.env.MODE || "public",  //public | private | inbox | group
-OWNER_NUMBER: process.env.OWNER_NUMBER || "84568370687",
-DEV: process.env.DEV || "84568370687",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "48729750345",
+DEV: process.env.DEV || "48729750345",
 LANG: process.env.LANG || "EN", // EN | SI
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
